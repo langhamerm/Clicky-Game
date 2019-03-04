f8f769c4e535a8b0b51e231c6a210fa0c2d7ee44
Mark Langhamer <langhamerm@gmail.com>
2019-03-04 17:55:53 -0600

    css

--- clicky-game/src/components/Footer/Footer.js
+++ clicky-game/src/components/Footer/Footer.js
@@ -2,7 +2,7 @@ import React from 'react';
 import "./Footer.css";
 const Footer = () => (
     <header className="footer">
-    <a href="https://github.com/langhamerm">Github Mark Langhamer</a>
+    <a href="https://github.com/langhamerm"><i class="fa fa-github"></i>Mark Langhamer</a>
     </header>
 );
 export default Footer;
