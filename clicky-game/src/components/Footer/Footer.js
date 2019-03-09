68e7192e48b9861454db3cad3ec8ec6439eb58ca
Mark Langhamer <langhamerm@gmail.com>
2019-03-09 09:43:07 -0600

    Updates

--- clicky-game/src/components/Footer/Footer.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import "./Footer.css";
-const Footer = () => (
-    <header className="footer">
-    <a href="https://github.com/langhamerm"><i class="fa fa-github"></i>Mark Langhamer</a>
-    </header>
-);
-export default Footer;
---
+++ precache-manifest.c7bf4d0f6ba8e0343fdc320b657bc06d.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = [
+  {
+    "revision": "6a60d22641e7452be7bcc4461a3a6e5f",
+    "url": "/Clicky-Game/static/media/flareon.6a60d226.png"
+  },
+  {
+    "revision": "d4919cbda0d2b3ca5516",
+    "url": "/Clicky-Game/static/css/main.e9f3acd4.chunk.css"
+  },
+  {
+    "revision": "61a0df336c60dbe48a52",
+    "url": "/Clicky-Game/static/js/runtime~main.61a0df33.js"
+  },
+  {
+    "revision": "646f3d2a74bef00b3a8f",
+    "url": "/Clicky-Game/static/js/2.646f3d2a.chunk.js"
+  },
+  {
+    "revision": "9ba4d684e64c19af7c99148ac36af46b",
+    "url": "/Clicky-Game/static/media/bulbasaur.9ba4d684.png"
+  },
+  {
+    "revision": "4e83c7dbbb912257f06b13cc1468f76f",
+    "url": "/Clicky-Game/static/media/charmeleon.4e83c7db.png"
+  },
+  {
+    "revision": "d6330a9d746c7a66f2eb27cec182ea43",
+    "url": "/Clicky-Game/static/media/dragonite.d6330a9d.png"
+  },
+  {
+    "revision": "c157545ab985be295c9566b427102231",
+    "url": "/Clicky-Game/static/media/eevee.c157545a.png"
+  },
+  {
+    "revision": "d4919cbda0d2b3ca5516",
+    "url": "/Clicky-Game/static/js/main.d4919cbd.chunk.js"
+  },
+  {
+    "revision": "e0737bed91857347aa412c9a0fde3a22",
+    "url": "/Clicky-Game/static/media/jolteon.e0737bed.png"
+  },
+  {
+    "revision": "fbc1ef4fed7b60445004d25f93ac5878",
+    "url": "/Clicky-Game/static/media/kadabra.fbc1ef4f.png"
+  },
+  {
+    "revision": "5bc90ea068bdd1162f0bf10a64326fe2",
+    "url": "/Clicky-Game/static/media/metapod.5bc90ea0.png"
+  },
+  {
+    "revision": "4e358ab74348b29e931c36756d9fe5c4",
+    "url": "/Clicky-Game/static/media/ninetales.4e358ab7.png"
+  },
+  {
+    "revision": "53dbfd304766b5858ee6e139646713c7",
+    "url": "/Clicky-Game/static/media/pikachu.53dbfd30.png"
+  },
+  {
+    "revision": "57d04bfe5867fc0a8e2b55000e7e5ee4",
+    "url": "/Clicky-Game/static/media/vaporeon.57d04bfe.png"
+  },
+  {
+    "revision": "04cfac8634923b57a079cc4381035686",
+    "url": "/Clicky-Game/static/media/wartortle.04cfac86.png"
+  },
+  {
+    "revision": "069dc8590d93194b0fe0e422196e381a",
+    "url": "/Clicky-Game/index.html"
+  }
+];
